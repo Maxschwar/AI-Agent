dd32020932ded9bcf90a907611c4eb80cf5ca40d
MaxSchwar <Maxschwarberg@aol.com>
2026-04-07 03:09:12 +0200

    A: added Google API to 'talk to'

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -8,14 +10,21 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("API Key did not load")
 client = genai.Client(api_key=api_key)
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
 
 def main():
-    print("Hello from agent!")
-    question = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    awnser = client.models.generate_content(model="gemini-2.5-flash", contents=question)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    awnser = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+
     if awnser.usage_metadata != None:
-        print(f"User prompt: {question}")
-        print(f"Prompt tokens: {awnser.usage_metadata.prompt_token_count} \nResponse tokens: {awnser.usage_metadata.candidates_token_count}")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {awnser.usage_metadata.prompt_token_count} \nResponse tokens: {awnser.usage_metadata.candidates_token_count}")
         print(f"Response:\n{awnser.text}")
     else: 
         raise RuntimeError("Awnser has no usage_metadata")
